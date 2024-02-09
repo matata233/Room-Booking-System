c6641e9b39fef33efc224e9138198239d6e46d55
hsiangyichen <hsiangyi1025@gmail.com>
2024-02-08 23:53:21 -0800

    feat: add mobile to admin landing page

--- frontend/src/pages/admin/AdminHomePage/AdminHomePage.js
+++ frontend/src/pages/admin/AdminHomePage/AdminHomePage.js
@@ -5,33 +5,35 @@ import UserIconSVG from "../../../assets/user-icon.svg";
 
 function AdminHomePage() {
   return (
-    <div className="flex h-[80vh] w-screen flex-col items-center justify-center">
-      <h1 className="mt-48 text-6xl">Booking Administration System</h1>
-      <div className="my-6 flex flex-col gap-4">
-        <div className="flex flex-row items-center">
+    <div className="flex h-auto w-screen flex-col items-center justify-center md:h-[80vh]">
+      <h1 className="mx-10 mt-24 text-4xl sm:mt-48 sm:text-5xl lg:text-6xl">
+        Booking Administration System
+      </h1>
+      <div className="my-6 hidden flex-col gap-4 sm:flex">
+        <div className="mx-10 flex flex-row items-center">
           <div className="min-w-16">
             <img src={RoomIconSVG} alt="Room Mngt Icon" className="h-10 w-16" />
           </div>
-          <p className="w-[770px]">
+          <p className="mx-10 w-auto md:w-[550px] lg:w-[770px]">
             Room Management allows the you to add/change/delete rooms into the
             system and record various features of the room.
           </p>
         </div>
-        <div className="flex flex-row items-center">
+        <div className="mx-10 flex flex-row items-center">
           <div className="min-w-16">
             <img src={UserIconSVG} alt="User Mngt Icon" className="h-10 w-16" />
           </div>
-          <p className="w-[775px]">
+          <p className="mx-10 w-auto md:w-[550px] lg:w-[775px]">
             Staff Management allows you to import and input user information (up
             to 1000 users) into the profile database and to perform
             change/delete staff information.
           </p>
         </div>
       </div>
-      <div className="flex flex-row gap-8">
-        <div className="flex w-8/12 flex-col items-center bg-white px-9 py-5 shadow-2xl transition-all duration-300 hover:translate-y-2 hover:shadow-2xl">
+      <div className="my-5 flex flex-col gap-5 md:flex-row md:gap-8">
+        <div className="mx-10 flex w-auto flex-col items-center bg-white px-5 py-5 shadow-2xl transition-all duration-300 hover:translate-y-2 hover:shadow-2xl md:mx-0 md:w-8/12 md:px-9">
           <img
-            className="h-60 object-cover"
+            className="h-40 object-cover md:h-60"
             alt="Meeting Room"
             src={RoomMngtSVG}
           />
@@ -46,9 +48,9 @@ function AdminHomePage() {
             </button>
           </div>
         </div>
-        <div className="flex w-8/12 flex-col items-center bg-white px-9 py-5 shadow-2xl transition-all duration-300 hover:translate-y-2 hover:shadow-2xl">
+        <div className="mx-10 flex w-auto flex-col items-center bg-white px-5 py-5 shadow-2xl transition-all duration-300 hover:translate-y-2 hover:shadow-2xl md:mx-0 md:w-8/12 md:px-9">
           <img
-            className="h-60 object-cover"
+            className="h-40 object-cover md:h-60"
             alt="Meeting Room"
             src={UserMngtSVG}
           />
