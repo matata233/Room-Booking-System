45dafbe37c97446b4e36dd928844fac9669f96ea
Carol Ma <carolma@Carols-MacBook-Pro.local>
2024-02-17 21:14:38 -0800

    Updated Equipments Select

--- frontend/src/components/AdminRoomForm.js
+++ frontend/src/components/AdminRoomForm.js
@@ -28,6 +28,10 @@ const AdminRoomForm = ({ firstlyHeader, secondaryHeader, buttonText }) => {
     event.preventDefault();
     // Handle form submission here
   };
+  const equipments = [
+    { id: "AV", description: "AV" },
+    { id: "CV", description: "CV" },
+  ];
 
   return (
     <ThemeProvider theme={PageTheme}>
@@ -144,74 +148,42 @@ const AdminRoomForm = ({ firstlyHeader, secondaryHeader, buttonText }) => {
                 />
               </div>
               <div className="relative flex items-center justify-start gap-4">
-                <div className="flex gap-x-4">
-                  <label
-                    className="relative flex cursor-pointer items-center rounded-full"
-                    htmlFor="checkAV"
-                  >
-                    <input
-                      type="checkbox"
-                      className="border-blue-gray-200 peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all  checked:border-theme-orange checked:bg-theme-orange "
-                      id="checkAV"
-                    />
-                    <span className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-theme-dark-blue opacity-0 transition-opacity peer-checked:opacity-100">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-3.5 w-3.5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-width="1"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                  </label>
-                  <label
-                    className="cursor-pointer select-none text-theme-dark-blue"
-                    htmlFor="checkAV"
-                  >
-                    AV
-                  </label>
-                </div>
-                <div className="flex gap-x-4">
-                  <label
-                    className="relative flex cursor-pointer items-center rounded-full"
-                    htmlFor="checkVC"
-                  >
-                    <input
-                      type="checkbox"
-                      className="border-blue-gray-200 peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all  checked:border-theme-orange checked:bg-theme-orange "
-                      id="checkVC"
-                    />
-                    <span className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-theme-dark-blue opacity-0 transition-opacity peer-checked:opacity-100">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-3.5 w-3.5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-width="1"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </span>
-                  </label>
-                  <label
-                    className="cursor-pointer select-none text-theme-dark-blue"
-                    htmlFor="checkVC"
-                  >
-                    VC
-                  </label>
-                </div>
+                {equipments.map((item) => (
+                  <div className="flex gap-x-4">
+                    <label
+                      className="relative flex cursor-pointer items-center rounded-full"
+                      htmlFor={item.id}
+                    >
+                      <input
+                        type="checkbox"
+                        className="border-blue-gray-200 peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border transition-all  checked:border-theme-orange checked:bg-theme-orange "
+                        id={item.id}
+                      />
+                      <span className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-theme-dark-blue opacity-0 transition-opacity peer-checked:opacity-100">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-3.5 w-3.5"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          stroke="currentColor"
+                          stroke-width="1"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </span>
+                    </label>
+                    <label
+                      className="cursor-pointer select-none text-theme-dark-blue"
+                      htmlFor={item.id}
+                    >
+                      {item.id}
+                    </label>
+                  </div>
+                ))}
               </div>
 
               <div className="relative">
