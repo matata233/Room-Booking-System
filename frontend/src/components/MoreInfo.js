1862ad7a088f599cd0047bb635170c60cce124eb
Carol Ma <carolma@Carols-MacBook-Pro.local>
2024-03-08 03:48:47 -0800

    Updated tooltip to make it works on mobiles

--- frontend/src/components/MoreInfo.js
+++ frontend/src/components/MoreInfo.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoInformationCircleOutline } from "react-icons/io5";
-import { IconButton, Tooltip } from "@mui/material";
+import { IconButton, Tooltip, ClickAwayListener } from "@mui/material";
 
 const MoreInfo = ({ info }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <Tooltip title={info}>
-      <IconButton>
-        <IoInformationCircleOutline />
-      </IconButton>
-    </Tooltip>
+    <>
+      {" "}
+      <div className="md:hidden">
+        <ClickAwayListener onClickAway={handleTooltipClose}>
+          <Tooltip
+            PopperProps={{
+              disablePortal: true,
+            }}
+            onClose={handleTooltipClose}
+            open={open}
+            title={info}
+            disableFocusListener
+            disableTouchListener
+            disableHoverListener
+          >
+            <IconButton onClick={handleTooltipOpen}>
+              <IoInformationCircleOutline />
+            </IconButton>
+          </Tooltip>
+        </ClickAwayListener>
+      </div>
+      <div className="hidden md:block">
+        <Tooltip title={info}>
+          <IconButton>
+            <IoInformationCircleOutline />
+          </IconButton>
+        </Tooltip>
+      </div>
+    </>
   );
 };
 
