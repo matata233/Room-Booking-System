7a95e764b56e52641d245461eea64a4e4993d42a
Carol Ma <carolma@dhcp-128-189-27-241.ubcsecure.wireless.ubc.ca>
2024-03-08 15:27:10 -0800

    Updated responsiveness

--- frontend/src/pages/BookingReviewPage.js
+++ frontend/src/pages/BookingReviewPage.js
@@ -8,41 +8,37 @@ const BookingReviewPage = () => {
     <div>
       <div className="flex w-full flex-col gap-y-12 font-amazon-ember">
         <BookingStepper />
-        <div className="mx-20 bg-white px-5 py-5 shadow-2xl transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl">
-          <div className="my-4 flex flex-col items-center justify-between xl:flex-row">
+
+        <div className="mx-6 bg-white px-5 py-5 shadow-2xl transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl">
+          <div className="flex flex-col items-center justify-between lg:flex-row">
             <div>
-              <div className="ml-10 flex flex-col items-center xl:flex-row">
-                <div>
-                  <img
-                    src={MeetingRoomImg}
-                    alt="meeting room"
-                    className="h-[25vh] object-cover"
-                  />
-                </div>
-                <div className="mt-6 flex flex-col xl:ml-6 xl:mt-0">
-                  {/* <div className="mt-2 text-lg text-theme-orange">room info</div> */}
-                  <div>
-                    <span className="font-semibold">Time:</span> 13:00 - 14:00
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Room:</span> 120
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Capacity:</span> 10
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Equipment includes:</span>{" "}
-                    CV
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Location:</span> Building
-                    112
-                  </div>
-                </div>
+              <img
+                src={MeetingRoomImg}
+                alt="meeting room"
+                className="h-[25vh] object-cover"
+              />
+            </div>
+            <div className="mt-6 flex flex-col  justify-start gap-1">
+              <div>
+                <span className="font-semibold">Time:</span> 13:00 - 14:00
+              </div>
+              <div>
+                <span className="font-semibold">Room:</span> 120
+              </div>
+              <div>
+                <span className="font-semibold">Capacity:</span> 10
+              </div>
+              <div>
+                <span className="font-semibold">Equipments:</span> CV
+              </div>
+              <div>
+                <span className="font-semibold">Location:</span> Building 112
               </div>
             </div>
-            <div className="ml-10 mr-10 mt-10 flex flex-col justify-end xl:mt-0">
-              <div className="mb-2 text-center font-semibold xl:text-left">
+
+            {/* Attendees */}
+            <div className="mt-10 flex flex-col lg:mt-0">
+              <div className="mb-2 text-center font-semibold">
                 <h2>Attendee(s):</h2>
               </div>
               <div className="h-32 w-80 overflow-y-auto rounded-lg bg-gray-200 pl-4 pt-2">
@@ -54,55 +50,44 @@ const BookingReviewPage = () => {
             </div>
           </div>
 
-          <div class="mx-4 my-4 hidden text-center text-theme-orange xl:block">
-            -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-          </div>
+          <div className="mx-auto  my-5 w-full border-t-2 border-dashed border-theme-orange"></div>
 
-          <div class="mx-4 my-4 block text-center text-theme-orange md:hidden">
-            --------------------------------------------------------------
-          </div>
-
-          <div class="mx-4 my-4 hidden text-center text-theme-orange md:block lg:block xl:hidden 2xl:hidden">
-            ------------------------------------------------------------------------------
-          </div>
-
-          <div className="my-4 flex flex-col items-center justify-between xl:flex-row">
+          <div className="flex flex-col items-center justify-between lg:flex-row">
             <div>
-              <div className="ml-10 flex flex-col items-center xl:flex-row">
-                <div>
-                  <img
-                    src={MeetingRoomImg}
-                    alt="meeting room"
-                    className="h-[25vh] object-cover"
-                  />
-                </div>
-                <div className="mt-6 flex flex-col xl:ml-6 xl:mt-0">
-                  <div>
-                    <span className="font-semibold">Time:</span> 13:00 - 14:00
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Room:</span> 420
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Capacity:</span> 4
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Equipment includes:</span>{" "}
-                    CV
-                  </div>
-                  <div className="mt-2">
-                    <span className="font-semibold">Location:</span> Building
-                    200
-                  </div>
-                </div>
+              <img
+                src={MeetingRoomImg}
+                alt="meeting room"
+                className="h-[25vh] object-cover"
+              />
+            </div>
+            <div className="mt-6 flex flex-col  justify-start gap-1">
+              <div>
+                <span className="font-semibold">Time:</span> 13:00 - 14:00
+              </div>
+              <div>
+                <span className="font-semibold">Room:</span> 120
+              </div>
+              <div>
+                <span className="font-semibold">Capacity:</span> 10
+              </div>
+              <div>
+                <span className="font-semibold">Equipments:</span> CV
+              </div>
+              <div>
+                <span className="font-semibold">Location:</span> Building 112
               </div>
             </div>
-            <div className="ml-10 mr-10 mt-10 flex flex-col justify-end xl:mt-0">
-              <div className="mb-2 text-center font-semibold xl:text-left">
+
+            {/* Attendees */}
+            <div className="mt-10 flex flex-col lg:mt-0">
+              <div className="mb-2 text-center font-semibold">
                 <h2>Attendee(s):</h2>
               </div>
-              <div className="h-32 w-80 overflow-y-auto rounded-lg bg-gray-200 pl-4 pt-2 ">
-                pig@gmail.com, bird@gmail.com, bear@gmail.com
+              <div className="h-32 w-80 overflow-y-auto rounded-lg bg-gray-200 pl-4 pt-2">
+                cat@gmail.com, dog@gmail.com, cat2@gmail.com, dog2@gmail.com,
+                cat3@gmail.com, dog3@gmail.com, cat4@gmail.com, dog4@gmail.com,
+                cat5@gmail.com, dog5@gmail.com, cat6@gmail.com, dog6@gmail.com,
+                cat7@gmail.com, dog7@gmail.com
               </div>
             </div>
           </div>
