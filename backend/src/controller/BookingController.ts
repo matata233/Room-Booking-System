e8f1b202dd7e792f91b73f72d1542bb5d09b1a2e
Jia Jun Liu <jliu065@students.cs.ubc.ca>
2024-03-12 21:39:44 -0700

    Booking Controller 1. fixed missing closing brackets
    Booking Repository
    2. fixed errors using Object instead of object  making assumption based on codes, REVIEW required):
    Don't use `Object` as a type. The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.
    - If you want a type meaning "any object", you probably want `object` instead.
    - If you want a type meaning "any value", you probably want `unknown` instead.
    - If you really want a type meaning "any non-nullish value", you probably want `NonNullable<unknown>` instead  @typescript-eslint/ban-types
    3. fixed errors using == instead of  ==
    4. fixed errors using != instead of !==
    5.removed Unnecessary escape character: \'
    
    Booking Service
    6. fixed errors using Object instead of object  making assumption based on codes, REVIEW required):
    Don't use `Object` as a type. The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.
    - If you want a type meaning "any object", you probably want `object` instead.
    - If you want a type meaning "any value", you probably want `unknown` instead.
    - If you really want a type meaning "any non-nullish value", you probably want `NonNullable<unknown>` instead  @typescript-eslint/ban-types
    7. fixed errors mixing  ?, ! assertion (REVIEW required)

--- backend/src/controller/BookingController.ts
+++ backend/src/controller/BookingController.ts
@@ -3,7 +3,12 @@ import RoomDTO from "../model/dto/RoomDTO";
 import UserDTO from "../model/dto/UserDTO";
 import BookingService from "../service/BookingService";
 import ResponseCodeMessage from "../util/enum/ResponseCodeMessage";
-import { BadRequestError, NotFoundError, RequestConflictError, UnavailableAttendeesError } from "../util/exception/AWSRoomBookingSystemError";
+import {
+    BadRequestError,
+    NotFoundError,
+    RequestConflictError,
+    UnavailableAttendeesError
+} from "../util/exception/AWSRoomBookingSystemError";
 import AbstractController from "./AbstractController";
 import {Request, Response} from "express";
 
@@ -18,6 +23,7 @@ export default class BookingController extends AbstractController {
     public getAll(req: Request, res: Response): Promise<Response> {
         return Promise.reject("Not implemented");
     }
+
     public getById(req: Request, res: Response): Promise<Response> {
         return Promise.reject("Not implemented");
     }
@@ -26,11 +32,11 @@ export default class BookingController extends AbstractController {
         const dto = new BookingDTO();
         dto.createdByUsername = req.body.createdByUsername!;
         dto.createdAt = new Date();
-        dto.startTime = new  Date( req.body.startTime! );
-        dto.endTime = new Date( req.body.endTime! );
-        dto.roomDTO = []
-        for( let entry of req.body.rooms ) {
-            let roomdto = new RoomDTO();
+        dto.startTime = new Date(req.body.startTime!);
+        dto.endTime = new Date(req.body.endTime!);
+        dto.roomDTO = [];
+        for (const entry of req.body.rooms) {
+            const roomdto = new RoomDTO();
             roomdto.roomId = entry;
             dto.roomDTO.push(roomdto);
         }
@@ -55,39 +61,43 @@ export default class BookingController extends AbstractController {
 
     public update(req: Request, res: Response): Promise<Response> {
         return Promise.reject("Not implemented");
-    } 
-
-    public getSuggestedTimes = async ( req: Request, res: Response ): Promise<Response> => {
-        let start_time = req.body.start_time;
-        let end_time = req.body.end_time;
-        let duration = req.body.duration;
-        let attendees = req.body.attendees;
-        let equipments = req.body.equipments;
-        let step_size = req.body.step_size;
-        
-        return this.bookingService.getSuggestedTimes( start_time, end_time, duration, attendees, equipments, step_size )
-        .then( ( time_slots ) => {
-            // return super.onReject( res, 400, "" );
-            return super.onResolve( res, time_slots );
-        })
-        .catch( ( err: BadRequestError ) => {
-            return super.onReject( res, ResponseCodeMessage.BAD_REQUEST_ERROR_CODE, err.message );
-        })
     }
 
-    public getAvailableRooms = async ( req: Request, res: Response ): Promise<Response> => {
-        let start_time = req.body.startTime!;
-        let end_time = req.body.endTime!;
-        let attendees = req.body.attendees!;
-        let equipments = req.body.equipments!;
-        let priority = req.body.priority!;
-        return this.bookingService.getAvailableRooms( start_time, end_time, attendees, equipments, priority )
-        .then( ( rooms ) => {
-            return super.onResolve( res, rooms );
-        })
-        .catch( ( err: NotFoundError ) => {
-            return super.onReject( res, ResponseCodeMessage.NOT_FOUND_CODE, err.message );
-        })
-        .catch( ( err: UnavailableAttendeesError ) => {
-            return super.onReject( res, ResponseCodeMessage.UNAVAILABLE_ATEENDEES, err.message );
-        });
+    public getSuggestedTimes = async (req: Request, res: Response): Promise<Response> => {
+        const start_time = req.body.start_time;
+        const end_time = req.body.end_time;
+        const duration = req.body.duration;
+        const attendees = req.body.attendees;
+        const equipments = req.body.equipments;
+        const step_size = req.body.step_size;
+
+        return this.bookingService
+            .getSuggestedTimes(start_time, end_time, duration, attendees, equipments, step_size)
+            .then((time_slots) => {
+                // return super.onReject( res, 400, "" );
+                return super.onResolve(res, time_slots);
+            })
+            .catch((err: BadRequestError) => {
+                return super.onReject(res, ResponseCodeMessage.BAD_REQUEST_ERROR_CODE, err.message);
+            });
+    };
+
+    public getAvailableRooms = async (req: Request, res: Response): Promise<Response> => {
+        const start_time = req.body.startTime!;
+        const end_time = req.body.endTime!;
+        const attendees = req.body.attendees!;
+        const equipments = req.body.equipments!;
+        const priority = req.body.priority!;
+        return this.bookingService
+            .getAvailableRooms(start_time, end_time, attendees, equipments, priority)
+            .then((rooms) => {
+                return super.onResolve(res, rooms);
+            })
+            .catch((err: NotFoundError) => {
+                return super.onReject(res, ResponseCodeMessage.NOT_FOUND_CODE, err.message);
+            })
+            .catch((err: UnavailableAttendeesError) => {
+                return super.onReject(res, ResponseCodeMessage.UNAVAILABLE_ATEENDEES, err.message);
+            });
+    };
+}
--- backend/src/service/BookingService.ts
+++ backend/src/service/BookingService.ts
@@ -7,45 +7,59 @@ import AggregateAttendeeDTO from "../model/dto/AggregateAttendeeDTO";
 export default class BookingService extends AbstractService {
     public bookingRepository: BookingRepository;
 
-    constructor( bookingRepository: BookingRepository ) {
+    constructor(bookingRepository: BookingRepository) {
         super();
         this.bookingRepository = bookingRepository;
     }
 
     public getAll(): Promise<BookingDTO[]> {
-        return Promise.reject( "Not Implemented" );
+        return Promise.reject("Not Implemented");
     }
 
     public getById(id: number): Promise<BookingDTO> {
-        return Promise.reject( "Not Implemented" );
+        return Promise.reject("Not Implemented");
     }
 
     public create(dto: BookingDTO): Promise<BookingDTO> {
         return this.bookingRepository.create(
             dto.createdByUsername!,
-            dto.createdAt?.toISOString()!,
-            dto.startTime?.toISOString()!,
-            dto.endTime?.toISOString()!,
-            dto.roomDTO?.map( entry => String(entry.roomId) )!,
-            dto.userDTOs?.map( entry => entry.username! )!
+            dto.createdAt!.toISOString(),
+            dto.startTime!.toISOString(),
+            dto.endTime!.toISOString(),
+            dto.roomDTO!.map((entry) => String(entry.roomId))!,
+            dto.userDTOs!.map((entry) => entry.username!)!
         );
-        return Promise.reject( "Not Implemented" );
     }
 
     public update(id: number, dto: BookingDTO): Promise<BookingDTO> {
-        return Promise.reject( "Not Implemented" );
+        return Promise.reject("Not Implemented");
     }
 
-    public getSuggestedTimes( start_time: string,
-                              end_time: string,
-                              duration: string,
-                              attendees: string[],
-                              equipments: string[],
-                              step_size: string ): Promise<Object> {
-        return this.bookingRepository.getSuggestedTimes( start_time, end_time, duration, attendees, equipments, step_size );
+    public getSuggestedTimes(
+        start_time: string,
+        end_time: string,
+        duration: string,
+        attendees: string[],
+        equipments: string[],
+        step_size: string
+    ): Promise<object> {
+        return this.bookingRepository.getSuggestedTimes(
+            start_time,
+            end_time,
+            duration,
+            attendees,
+            equipments,
+            step_size
+        );
     }
 
-    public getAvailableRooms( start_time: string, end_time: string, attendees: string[], equipments: string[], priority: string[]): Promise<Object> {
-        return this.bookingRepository.getAvailableRooms( start_time, end_time,attendees,equipments,priority);
+    public getAvailableRooms(
+        start_time: string,
+        end_time: string,
+        attendees: string[],
+        equipments: string[],
+        priority: string[]
+    ): Promise<object> {
+        return this.bookingRepository.getAvailableRooms(start_time, end_time, attendees, equipments, priority);
     }
 }
