b122de04b86ddddc57e0eb8a0b6448b6aa0df49a
Carol Ma <carolma@Carols-MacBook-Pro.local>
2024-03-14 11:18:27 -0700

    Updated TimeDropdown state

--- frontend/src/components/TimeDropdown.js
+++ frontend/src/components/TimeDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import DropdownArrowSVG from "../assets/dropdown-arrow.svg";
@@ -8,7 +8,13 @@ import dayjs from "dayjs";
 
 const TimeDropdowns = () => {
   const dispatch = useDispatch();
-  const booking = useSelector((state) => state.booking);
+  const { startDate, startTime, endTime } = useSelector(
+    (state) => state.booking,
+  );
+
+  const handleDateChange = (e) => {
+    dispatch(setStartDate(e.target.value));
+  };
 
   const handleStartTimeChange = (e) => {
     dispatch(setStartTime(e.target.value));
@@ -18,56 +24,92 @@ const TimeDropdowns = () => {
     dispatch(setEndTime(e.target.value));
   };
 
-  const handleStartDateChange = (e) => {
-    const date = e.target.value;
-    dispatch(setStartDate(e.target.value));
-    // const inputValue = e.target.value;
-
-    // const selectedDate = dayjs(inputValue || undefined)
-    //   .startOf("day")
-    //   .toDate(); // Get 12AM of time
-
-    // dispatch(setStartDate(selectedDate));
+  const generateTimeOptions = () => {
+    const options = [];
+    for (let i = 0; i < 24; i++) {
+      for (let j = 0; j < 60; j += 15) {
+        const hour = i < 10 ? `0${i}` : `${i}`;
+        const minute = j === 0 ? "00" : `${j}`;
+        options.push(`${hour}:${minute}`);
+      }
+    }
+    return options;
   };
+
+  function getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  const validateEndTime = () => {
+    const startDateTime = dayjs(`${startDate} ${startTime}`);
+    const endDateTime = dayjs(`${startDate} ${endTime}`);
+    if (endDateTime.isBefore(startDateTime)) {
+      alert("End time should be later than start time.");
+    }
+  };
+
+  useEffect(() => {
+    validateEndTime();
+  }, [startTime, endTime, startDate]);
 
   return (
     <div className="flex w-80 flex-col rounded-lg bg-gray-200 p-4">
-      <div className="mb-2">
-        Date<span className="text-red-600">*</span>
-      </div>
+      <div>Date</div>
       <input
         type="date"
-        value={booking.startDate}
-        onChange={handleStartDateChange}
-        className="mb-4 block w-full appearance-none rounded-md bg-white px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-        aria-label="Date"
+        value={startDate}
+        min={getCurrentDate()}
+        onChange={handleDateChange}
+        className="mb-4 block w-full cursor-pointer appearance-none rounded-md bg-white px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
         required
       />
-      {console.log(booking.startDate)}
-
-      <div className="mb-2">
-        Start Time<span className="text-red-600">*</span>
+      <div>Start Time</div>
+      <div className="relative">
+        <select
+          className="block w-full cursor-pointer appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+          value={startTime}
+          onChange={handleStartTimeChange}
+        >
+          {generateTimeOptions().map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <img
+            src={DropdownArrowSVG}
+            alt="Dropdown Arrow"
+            className="h-5 w-5"
+          />
+        </div>
       </div>
-      <input
-        type="time"
-        value={booking.startTime}
-        onChange={handleStartTimeChange}
-        className="mb-4 block w-full appearance-none rounded-md bg-white px-4 py-2  leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-        aria-label="Start Time"
-        required
-      />
 
-      <div className="mb-2 mt-4">
-        End Time<span className="text-red-600">*</span>
+      <div className="mt-4">End Time</div>
+      <div className="relative mb-4">
+        <select
+          className="block w-full cursor-pointer appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+          value={endTime}
+          onChange={handleEndTimeChange}
+        >
+          {generateTimeOptions().map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <img
+            src={DropdownArrowSVG}
+            alt="Dropdown Arrow"
+            className="h-5 w-5"
+          />
+        </div>
       </div>
-      <input
-        type="time"
-        value={booking.endTime}
-        onChange={handleEndTimeChange}
-        className="mb-4 block w-full appearance-none rounded-md bg-white px-4 py-2  leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-        aria-label="End Time"
-        required
-      />
     </div>
   );
 };
