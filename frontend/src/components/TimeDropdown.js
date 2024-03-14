478190fb886a25cc8dfd45d74c9b974648e7d038
hsiangyichen <hsiangyi1025@gmail.com>
2024-03-14 00:54:21 -0700

    fix: wrong show time and add cursor pointer

--- frontend/src/components/TimeDropdown.js
+++ frontend/src/components/TimeDropdown.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import React, { useState, useEffect } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import DropdownArrowSVG from "../assets/dropdown-arrow.svg";
+import dayjs from "dayjs";
 
 const TimeDropdowns = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
+  const [startDate, setStartDate] = useState(
+    dayjs(new Date()).format("YYYY-MM-DD"),
+  );
+  const [startTime, setStartTime] = useState("00:00");
+  const [endTime, setEndTime] = useState("23:45");
 
   const handleStartTimeChange = (e) => {
     setStartTime(e.target.value);
@@ -28,20 +30,41 @@ const TimeDropdowns = () => {
     return options;
   };
 
+  function getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  const validateEndTime = () => {
+    const startDateTime = dayjs(`${startDate} ${startTime}`);
+    const endDateTime = dayjs(`${startDate} ${endTime}`);
+    if (endDateTime.isBefore(startDateTime)) {
+      alert("End time should be later than start time.");
+    }
+  };
+
+  useEffect(() => {
+    validateEndTime();
+  }, [startTime, endTime]);
+
   return (
     <div className="flex w-80 flex-col rounded-lg bg-gray-200 p-4">
       <div>Date</div>
-      <DatePicker
-        selected={startDate}
-        minDate={new Date()}
-        onChange={(date) => setStartDate(date)}
-        dateFormat="MMMM d, yyyy"
-        className="mb-4 block w-full appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+      <input
+        type="date"
+        value={startDate}
+        min={getCurrentDate()}
+        onChange={(e) => setStartDate(e.target.value)}
+        className="mb-4 block w-full cursor-pointer appearance-none rounded-md bg-white px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+        required
       />
       <div>Start Time</div>
       <div className="relative">
         <select
-          className="block w-full appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+          className="block w-full cursor-pointer appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           value={startTime}
           onChange={handleStartTimeChange}
         >
@@ -63,7 +86,7 @@ const TimeDropdowns = () => {
       <div className="mt-4">End Time</div>
       <div className="relative mb-4">
         <select
-          className="block w-full appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+          className="block w-full cursor-pointer appearance-none rounded-md bg-white px-4 py-2 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           value={endTime}
           onChange={handleEndTimeChange}
         >
