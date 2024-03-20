c354dc5025b5c5f585bb044738896fb1d67533bd
yli0402 <77415930+yli0402@users.noreply.github.com>
2024-03-19 23:52:20 -0700

    integration finished!

--- frontend/src/pages/BookingHistoryPage.js
+++ frontend/src/pages/BookingHistoryPage.js
@@ -2,9 +2,19 @@ import React, { useState, useMemo } from "react";
 import MeetingRoomImg from "../assets/meeting-room.jpg";
 import { Link } from "react-router-dom";
 import { useGetBookingCurrentUserQuery } from "../slices/bookingApiSlides";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { useSelector } from "react-redux";
+import CheckIcon from '@mui/icons-material/Check';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const BookingHistoryPage = () => {
-  const { data: booking, error, isLoading, refetch } = useGetBookingCurrentUserQuery();
+  const {
+    data: booking,
+    error,
+    isLoading,
+    refetch,
+  } = useGetBookingCurrentUserQuery();
 
   const bookingData = useMemo(() => {
     if (isLoading || !booking || !booking.result) {
@@ -14,97 +24,141 @@ const BookingHistoryPage = () => {
     return booking.result;
   }, [isLoading, booking]);
 
-  console.log("my",booking)
+  console.log("my", bookingData);
+
+  function formatDateTime(startTime, endTime) {
+    const startDate = new Date(startTime);
+    const endDate = new Date(endTime);
+
+    const formattedDate = startDate.toISOString().split("T")[0];
+
+    const formattedStartTime = startDate
+      .toISOString()
+      .split("T")[1]
+      .substring(0, 5);
+
+    const formattedEndTime = endDate
+      .toISOString()
+      .split("T")[1]
+      .substring(0, 5);
+
+    return {
+      date: formattedDate,
+      startTime: formattedStartTime,
+      endTime: formattedEndTime,
+    };
+  }
+
+  const { userInfo } = useSelector((state) => state.auth)
 
   return (
     <div>
       <div className="flex w-full flex-col gap-y-12 font-amazon-ember">
-      <h1 className="text-2xl text-center font-semibold">
-          Booking History
-        </h1>
-        <div className="mx-6 mb-10 bg-white px-5 py-5 shadow-2xl transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl">
-          <div className="flex flex-col items-center justify-between lg:flex-row">
-            <div>
-              <img
-                src={MeetingRoomImg}
-                alt="meeting room"
-                className="h-[25vh] object-cover"
-              />
-            </div>
-            <div className="mt-6 flex flex-col  justify-start gap-1">
-              <div>
-                <span className="font-semibold">Time:</span> 13:00 - 14:00
-              </div>
-              <div>
-                <span className="font-semibold">Room:</span> 120
-              </div>
-              <div>
-                <span className="font-semibold">Capacity:</span> 10
-              </div>
-              <div>
-                <span className="font-semibold">Equipments:</span> CV
-              </div>
-              <div>
-                <span className="font-semibold">Location:</span> Building 112
-              </div>
-            </div>
+        <h1 className="text-center text-2xl font-semibold">Booking History</h1>
 
-            {/* Attendees */}
-            <div className="mt-10 flex flex-col lg:mt-0">
-              <div className="mb-2 text-center font-semibold">
-                <h2>Attendee(s):</h2>
-              </div>
-              <div className="h-32 w-80 overflow-y-auto rounded-lg bg-gray-200 pl-4 pt-2">
-                cat@gmail.com, dog@gmail.com, cat2@gmail.com, dog2@gmail.com,
-                cat3@gmail.com, dog3@gmail.com, cat4@gmail.com, dog4@gmail.com,
-                cat5@gmail.com, dog5@gmail.com, cat6@gmail.com, dog6@gmail.com,
-                cat7@gmail.com, dog7@gmail.com
-              </div>
-            </div>
-          </div>
+        {bookingData.length > 0 ? (
+          <>
+            <div className="flex flex-col gap-4">
+              {bookingData.map((book) => (
+                <div
+                  key={book.bookingId}
+                  className="mx-6 mb-10 bg-white px-5 py-5 shadow-2xl transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl"
+                >
+                  {/* ----- */}
+                  {book.groups.map((group, index) => (
+                    <div>
+                      <div
+                        key={group.room.roomId}
+                        className="flex flex-col items-center justify-between lg:flex-row"
+                      >
+                        <div>
+                          <div className="mb-2 mt-2 text-center text-lg font-semibold">
+                            {book.status == "confirmed"? <div className="text-green-500"> Confirmed <CheckIcon/> </div> :  <div className="text-red-500"> Canceled <CancelIcon/> </div> }
+                          </div>
+                          <img
+                            src={MeetingRoomImg}
+                            alt="meeting room"
+                            className="h-[25vh] object-cover"
+                          />
+                        </div>
+                        <div className="mt-6 flex flex-col  justify-start gap-1">
+                          <div className="mt-2 text-lg text-theme-orange">
+                            {`${group.room.city.cityId}${group.room.building.code} ${group.room.floorNumber.toString().padStart(2, "0")}.${group.room.roomCode} ${group.room.roomName ? group.room.roomName : ""} `}{" "}
+                          </div>
+                          <div>
+                            <span className="font-semibold">Time:</span>{" "}
+                            {`${formatDateTime(book.startTime, book.endTime).date}` +
+                              " " +
+                              `${formatDateTime(book.startTime, book.endTime).startTime}` +
+                              " - " +
+                              `${formatDateTime(book.startTime, book.endTime).endTime}`}
+                          </div>
+                          <div className="mt-2">
+                            <span className="font-semibold">Equipments:</span>{" "}
+                            {group.room.equipmentList.hasAV &&
+                            group.room.equipmentList.hasVC
+                              ? "AV / VC"
+                              : group.room.equipmentList.hasAV
+                                ? "AV"
+                                : group.room.equipmentList.hasVC
+                                  ? "VC"
+                                  : "None"}
+                          </div>
+                          <div className="mt-2">
+                            <span className="font-semibold">
+                              Number of Seats:
+                            </span>{" "}
+                            {group.room.numberOfSeats}
+                          </div>
+                        </div>
 
-          <div className="mx-auto  my-5 w-full border-t-2 border-dashed border-theme-orange"></div>
-
-          <div className="flex flex-col items-center justify-between lg:flex-row">
-            <div>
-              <img
-                src={MeetingRoomImg}
-                alt="meeting room"
-                className="h-[25vh] object-cover"
-              />
+                        {/* Attendees */}
+                        <div className="mt-10 flex flex-col lg:mt-0">
+                          <div className="mb-2 text-center font-semibold">
+                            <h2>Attendee(s):</h2>
+                          </div>
+                          <div className="h-32 w-80 overflow-y-auto rounded-lg bg-gray-200 pl-4 pt-2">
+                            {group.attendees.map((attendee) => (
+                              <div className="flex items-center">
+                                <div className="h-2 w-2 rounded-full bg-theme-orange"></div>
+                                <div className="ml-2">{attendee.email}</div>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                      {book.groups.length > 1 &&
+                        index < book.groups.length - 1 && (
+                          <>
+                            <div className="break"></div>
+                            <div className="flex-item mx-auto  my-5 w-full border-t-2 border-dashed border-theme-orange"></div>
+                          </>
+                        )}
+                    </div>
+                  ))}
+                  {book.status == "confirmed" && userInfo.email == book.users.email && (<div className="flex justify-end">
+                  <div className="flex space-x-6 ">
+                    <button
+                      className="text-indigo-600 hover:text-indigo-900 "
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      <FaEdit className="size-5" />
+                    </button>
+                    <button
+                      className="text-red-600 hover:text-red-900"
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      <MdDelete className="size-5" />
+                    </button>
+                  </div>
+                  </div>)}
+                </div>
+              ))}
             </div>
-            <div className="mt-6 flex flex-col  justify-start gap-1">
-              <div>
-                <span className="font-semibold">Time:</span> 13:00 - 14:00
-              </div>
-              <div>
-                <span className="font-semibold">Room:</span> 120
-              </div>
-              <div>
-                <span className="font-semibold">Capacity:</span> 10
-              </div>
-              <div>
-                <span className="font-semibold">Equipments:</span> CV
-              </div>
-              <div>
-                <span className="font-semibold">Location:</span> Building 112
-              </div>
-            </div>
-
-            {/* Attendees */}
-            <div className="mt-10 flex flex-col lg:mt-0">
-              <div className="mb-2 text-center font-semibold">
-                <h2>Attendee(s):</h2>
-              </div>
-              <div className="h-32 w-80 overflow-y-auto rounded-lg bg-gray-200 pl-4 pt-2">
-                cat@gmail.com, dog@gmail.com, cat2@gmail.com, dog2@gmail.com,
-                cat3@gmail.com, dog3@gmail.com, cat4@gmail.com, dog4@gmail.com,
-                cat5@gmail.com, dog5@gmail.com, cat6@gmail.com, dog6@gmail.com,
-                cat7@gmail.com, dog7@gmail.com
-              </div>
-            </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <div className="mt-20 text-center">You have no booking history.</div>
+        )}
       </div>
     </div>
   );
