f43a2864c2fe24f05f3343cdbbeac2d9f1063f6d
Carol Ma <carolma@Carols-MacBook-Pro.local>
2024-03-21 12:37:05 -0700

    Finished edit room/user

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -87,7 +87,7 @@ app.get(`${endpoint}/booking`, bookingController.getAll);
             "end_time": "2024-03-26T03:30:00.000Z"
         },...
 */
-app.get(`${endpoint}/booking/time-suggestion`, bookingController.getSuggestedTimes);
+app.post(`${endpoint}/booking/time-suggestion`, bookingController.getSuggestedTimes);
 /*
     Currently taking the following input as parameter:
     {
