76fd6f1b4235470b2877cb417e8ebb076add4e42
Yingkai (David) Zhao <75595017+yingkaizhao@users.noreply.github.com>
2024-03-21 15:52:16 -0700

    0. Integrate multi-room participants auto-splitting logic 1. Use class-transformer/validator to simplify DTO mapping and input validation (for rooms only now) 2. Update init.sql & Prisma schema to make all NULL rooms names empty strings instead and require the field to be NOT NULL 3. Update init.sql to add back all distance entries and add more mock user accounts 4. Update package.json to remove supertest, add class-transformer/validator, chai-subset, and a "test" script for testing 5. Add more tests 6. Enhanced error messages 7. Add generic error handling helper to AbstractController 8. Add generic DTO validation helper to AbstractService 9. Add equipment info for booking DTO mapper 10. Project-wide code clean-up and refactoring

--- backend/src/service/BookingService.ts
+++ backend/src/service/BookingService.ts
@@ -4,7 +4,7 @@ import BookingRepository from "../repository/BookingRepository";
 import {BadRequestError} from "../util/exception/AWSRoomBookingSystemError";
 
 export default class BookingService extends AbstractService {
-    public bookingRepository: BookingRepository;
+    private bookingRepository: BookingRepository;
 
     constructor(bookingRepository: BookingRepository) {
         super();
@@ -103,8 +103,16 @@ export default class BookingService extends AbstractService {
         end_time: string,
         attendees: string[],
         equipments: string[],
-        priority: string[]
+        priority: string[],
+        num_rooms: number
     ): Promise<object> {
-        return this.bookingRepository.getAvailableRooms(start_time, end_time, attendees, equipments, priority);
+        return this.bookingRepository.getAvailableRooms(
+            start_time,
+            end_time,
+            attendees,
+            equipments,
+            priority,
+            num_rooms
+        );
     }
 }
