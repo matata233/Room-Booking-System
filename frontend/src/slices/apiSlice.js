58574a3e274ba5ae8319621e5581e44a03b1c0a7
dorager91 <dorothy.unicorn@gmail.com>
2024-03-21 15:27:28 -0700

    force-logout feature finished implementation

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -11,10 +11,16 @@ const baseQuery = fetchBaseQuery({
     const token = getState().auth?.userInfo?.token;
     if (token) {
       if (Date.now() >= jwtDecode(token).exp * 1000) {
+        console.log("checking expiration", jwtDecode(token).exp);
+        localStorage.setItem("userInfo", null);
+        console.log("userInfo: ", localStorage.getItem("userInfo")?
+            localStorage.getItem("userInfo"): "successfully set to null" );
         toast.error(
             `Your token has expired, please login again.`
         );
-        localStorage.setItem("userInfo", null);
+        const event = new CustomEvent('token-expired');
+        console.log("new event");
+        window.dispatchEvent(event);
       }
       headers.set("authorization", `Bearer ${token}`);
       console.log("token", token);
