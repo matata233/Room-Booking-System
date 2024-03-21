a524360b87f60bbf664cd07d51a47388a20e4925
dorager91 <dorothy.unicorn@gmail.com>
2024-03-21 15:31:54 -0700

    removed some log statements

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -11,15 +11,11 @@ const baseQuery = fetchBaseQuery({
     const token = getState().auth?.userInfo?.token;
     if (token) {
       if (Date.now() >= jwtDecode(token).exp * 1000) {
-        console.log("checking expiration", jwtDecode(token).exp);
         localStorage.setItem("userInfo", null);
-        console.log("userInfo: ", localStorage.getItem("userInfo")?
-            localStorage.getItem("userInfo"): "successfully set to null" );
         toast.error(
             `Your token has expired, please login again.`
         );
         const event = new CustomEvent('token-expired');
-        console.log("new event");
         window.dispatchEvent(event);
       }
       headers.set("authorization", `Bearer ${token}`);
