81ec71915fa0b11a47a43e125bae6100d43d4017
Carol Ma <carolma@Carols-MacBook-Pro.local>
2024-03-23 16:11:38 -0700

    Finished csv upload

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
+import { useUploadUserCSVMutation } from "../slices/usersApiSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import sampleCSV from "../assets/sampleCSV.csv";
 
 const FileUpload = () => {
+  const navigate = useNavigate();
   const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadUserCSV, { isLoading, error }] = useUploadUserCSVMutation();
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    setSelectedFile(file);
+    if (file.type === "text/csv" || file.name.endsWith(".csv")) {
+      setSelectedFile(file);
+    } else {
+      alert("Only CSV files are allowed");
+      setSelectedFile(null);
+    }
   };
 
   const handleSubmit = async () => {
@@ -15,11 +26,18 @@ const FileUpload = () => {
       return;
     }
 
-    //prepare the file to be sent to the server
+    // prepare the file to be sent to the server
     const formData = new FormData();
     formData.append("file", selectedFile);
-
-    // TODO: POST the form data to the server
+    console.log("formData", formData);
+    try {
+      const response = await uploadUserCSV(formData).unwrap();
+      toast.success(`${selectedFile.name} uploaded successfully!`);
+      navigate("/userManagementPage");
+    } catch (err) {
+      toast.error(err?.data?.error || "Failed to upload file");
+      console.error("Failed to upload file:", err);
+    }
   };
 
   return (
@@ -34,7 +52,7 @@ const FileUpload = () => {
             <span className="font-semibold">Click to upload</span> or drag and
             drop
           </p>
-          <p className="text-xs text-gray-500">CSV, XLS or XLSX files only</p>
+          <p className="text-xs text-gray-500">CSV file only</p>
           {selectedFile && (
             <p className="mt-4 text-sm font-medium text-theme-blue">
               {selectedFile.name}
@@ -46,7 +64,7 @@ const FileUpload = () => {
           type="file"
           className="hidden"
           onChange={handleFileChange}
-          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+          accept=".csv, text/csv"
         />
       </label>
       <button
@@ -55,6 +73,13 @@ const FileUpload = () => {
       >
         Upload
       </button>
+      <a
+        href={sampleCSV}
+        download="sampleCSV.csv"
+        className="cursor-pointer rounded-lg bg-theme-dark-blue px-2 py-1 text-sm text-white transition-colors duration-300 ease-in-out hover:bg-theme-blue hover:text-white md:px-4 md:py-2 md:text-base"
+      >
+        Download Sample File
+      </a>
     </div>
   );
 };
