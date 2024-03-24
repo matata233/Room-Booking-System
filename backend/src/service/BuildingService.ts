927fb7547afd5b73a2978408b41c3e9eab1938a5
Yingkai (David) Zhao <75595017+yingkaizhao@users.noreply.github.com>
2024-03-23 22:53:15 -0700

    Finished create building API with distance calculations

--- backend/src/service/BuildingService.ts
+++ backend/src/service/BuildingService.ts
@@ -1,9 +1,11 @@
 import AbstractService from "./AbstractService";
 import BuildingRepository from "../repository/BuildingRepository";
 import BuildingDTO from "../model/dto/BuildingDTO";
+import {BadRequestError, NotFoundError, RequestConflictError} from "../util/exception/AWSRoomBookingSystemError";
+import {PrismaClientKnownRequestError} from "@prisma/client/runtime/library";
 
 export default class BuildingService extends AbstractService {
-    private buildingRepo: BuildingRepository; // The repository for the Building model
+    private buildingRepo: BuildingRepository;
 
     constructor(buildingRepo: BuildingRepository) {
         super();
@@ -11,15 +13,31 @@ export default class BuildingService extends AbstractService {
     }
 
     public async getAll(): Promise<BuildingDTO[]> {
-        return this.buildingRepo.findAll(); // Get all buildings. Data type: BuildingDTO[]
+        return this.buildingRepo.findAll();
     }
 
     public getById(id: number): Promise<BuildingDTO> {
+        if (isNaN(id)) {
+            throw new BadRequestError("invalid building ID");
+        }
         return this.buildingRepo.findById(id);
     }
 
-    public create(dto: BuildingDTO): Promise<BuildingDTO> {
-        return Promise.reject("Not implemented");
+    public async create(dto: BuildingDTO): Promise<BuildingDTO> {
+        await this.validateDTO(dto);
+        try {
+            return await this.buildingRepo.create(dto);
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === "P2002") {
+                    throw new RequestConflictError(`building ${dto.code} already exists in ${dto.city?.cityId}`);
+                }
+                if (error.code === "P2003") {
+                    throw new NotFoundError(`city ${dto.city?.cityId} does not exist`);
+                }
+            }
+            throw error;
+        }
     }
 
     public update(id: number, dto: BuildingDTO): Promise<BuildingDTO> {
